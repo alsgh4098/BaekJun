9488f12080123038f0beb396969eb8a41708af2c
진민호 <@DESKTOP-PAL57U0>
2020-05-05 17:11:24 +0900

    0505

--- BaekJoon2/src/Main_알고스팟_gold4.java
+++ BaekJoon2/src/Main_알고스팟_gold4.java
@@ -1,46 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Queue;
-import java.util.Scanner;
-
-import oracle.jrockit.jfr.tools.ConCatRepository;
 
 public class Main_알고스팟_gold4 {
 
 	static int[] dx = { -1, 0, 1, 0 };
 	static int[] dy = { 0, 1, 0, -1 };
 
-	static class Node implements Comparable<Node> {
+	static class Node{
 		int x;
 		int y;
 		int dist;
+		int block;
 
-		public Node(int x, int y, int dist) {
+		public Node(int x, int y, int dist, int block) {
 			super();
 			this.x = x;
 			this.y = y;
 			this.dist = dist;
-		}
-
-		@Override
-		public int compareTo(Node o) {
-			if (this.dist > o.dist) {
-				return 1;
-			} else {
-				return -1;
-			}
+			this.block = block;
 		}
 
 	}
 
 	static int[][] room;
-	static int[][] dist;
-	static boolean[][] visited;
-	static PriorityQueue<Node> qu;
+	static boolean[][][] visited;
+	static Queue<Node> qu;
+	static int min;
 	static String[] line;
 	static String line2;
 
@@ -48,10 +36,12 @@ public class Main_알고스팟_gold4 {
 //		Scanner sc = new Scanner(System.in);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		line = br.readLine().split(" ");
-		int M = Integer.parseInt(line[0]);
-		int N = Integer.parseInt(line[1]);
+		int N = Integer.parseInt(line[0]);
+		int M = Integer.parseInt(line[1]);
+		int K = Integer.parseInt(line[2]);
 
 		room = new int[N][M];
+		visited = new boolean[N][M][11];
 
 		for (int i = 0; i < N; i++) {
 			line2 = br.readLine();
@@ -60,47 +50,49 @@ public class Main_알고스팟_gold4 {
 			}
 		} // end input
 
-		visited = new boolean[N][M];
-		dist = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			Arrays.fill(dist[i], Integer.MAX_VALUE);
-		}
-		dist[0][0] = 0;
-		qu = new PriorityQueue<Node>();
-		qu.add(new Node(0, 0, dist[0][0]));
+		min = Integer.MAX_VALUE;
+		qu = new LinkedList<Node>();
+		
+		// 벽을 몇번 부수고 왔는지.
+		visited[0][0][0] = true;
+		// x y dist k
+		qu.add(new Node(0, 0, 1, 0));
 
-		// dijkstra + BFS
+		// BFS
 		while (!qu.isEmpty()) {
 			Node ths = qu.poll();
-//			System.out.println(ths.x +" "+ths.y);
-			if (ths.x == N && ths.y == M ) {
-				continue;
+
+			if (ths.x == N - 1 && ths.y == M - 1) {
+				min = Math.min(ths.dist, min);
+				break;
 			}
 
 			for (int i = 0; i < 4; i++) {
 				int nx = ths.x + dx[i];
 				int ny = ths.y + dy[i];
 				if (0 <= nx && nx < N && 0 <= ny && ny < M) {
-					if (dist[nx][ny] > dist[ths.x][ths.y] + room[nx][ny]) {
-						dist[nx][ny] = dist[ths.x][ths.y] + room[nx][ny];
-//						System.out.println(nx+" "+ny);
-						qu.add(new Node(nx, ny, dist[nx][ny]));
+					
+					if (room[nx][ny] == 1) {
+						if (ths.block < K && visited[nx][ny][ths.block + 1] == false) {
+							visited[nx][ny][ths.block+1] = true;
+							qu.add(new Node(nx, ny, ths.dist + 1, ths.block + 1));
+						}
+					} else {
+						if (visited[nx][ny][ths.block] == false) {
+							visited[nx][ny][ths.block] = true;
+							qu.add(new Node(nx, ny, ths.dist + 1, ths.block));
+						}
+
 					}
+
 				}
 			}
 
 		}
-		
-//		for (int i = 0; i < N; i++) {
-//			for (int j = 0; j < M; j++) {
-//				System.out.print(dist[i][j]+" ");
-//			}
-//			System.out.println();
-//		}
 
 		// 0 1 2 3 4 5 6 7 8 9
 
-		System.out.println(dist[N-1][M-1]);
+		System.out.println( min == Integer.MAX_VALUE ? -1 : min);
 
 	}
 
